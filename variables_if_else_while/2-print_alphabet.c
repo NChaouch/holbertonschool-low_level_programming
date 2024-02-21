8abb49482baef21e4f92aa0984b919d5f96a093b
NChaouch <nourchaouch44@gmail.com>
2024-02-21 14:19:43 +0000

    le 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz");
+	putchar("\n");
+	return (0);
+}

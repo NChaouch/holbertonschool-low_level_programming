5ecd054597b32e2a0c64e562ed1f71e163d5b19b
NChaouch <nourchaouch44@gmail.com>
2024-02-23 13:19:17 +0000

    le 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: program to print a string followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}

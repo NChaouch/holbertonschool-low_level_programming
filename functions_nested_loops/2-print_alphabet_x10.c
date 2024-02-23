16e9901b19b298070d5bd9a3985df92e56fdb925
NChaouch <nourchaouch44@gmail.com>
2024-02-23 16:40:25 +0000

    Fix task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
- *
  * print_alphabet_x10 - print alphabet x10
  *
  * Return: 0
- *
- *
  **/
-
 void print_alphabet_x10(void)
 {
 	int num;
 	char alphabet;
+
 	for (num = 0; num <= 9; num++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)

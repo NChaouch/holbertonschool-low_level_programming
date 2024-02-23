4faba68c2392fa7648050b6c8a243c1c1616339f
NChaouch <nourchaouch44@gmail.com>
2024-02-23 15:13:15 +0000

    le 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: 0
+ *
+ *
+ **/
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char alphabet;
+	for (num = 0; num <= 9; num++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+	_putchar('\n');
+	}
+}

d9e04cda80479248a3cb998f638339825d4096f3
NChaouch <nourchaouch44@gmail.com>
2024-02-25 21:43:18 +0000

    le 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @int1: integer 1
+ *
+ * @int2: integer 2
+ *
+ * Return: somme to integers
+ *
+ */
+int add(int int1, int int2)
+{
+	int result;
+
+	result = int1 + int2;
+	return (result);
+}
+

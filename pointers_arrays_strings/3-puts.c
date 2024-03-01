2b4c3ba9125f6cd2c0d59f0f04d0153d36f643da
NChaouch <nourchaouch44@gmail.com>
2024-03-01 10:22:25 +0000

    le 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts -  prints a string, followed by a new line, to stdout
+ *@str: character
+ *
+ *
+*/
+
+void _puts(char *str)
+{
+	int print;
+
+	for (print = 0; str[print] != '\0'; print++)
+
+	{
+		_putchar(print);
+	}
+}

de02e59a647d5a9e3da3eae2b29aa9b8d97972bb
NChaouch <nourchaouch44@gmail.com>
2024-03-04 21:20:22 +0000

    le 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,11 +8,12 @@
 
 /** 
  * fichier de configuration + appel de librairie 
- * avec les fonctions de rappele
+ * avec les fonctions de rappel
  */
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif

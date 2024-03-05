e376d27869a187c2bdb1547e3ea63b167ef14ac5
NChaouch <nourchaouch44@gmail.com>
2024-03-05 09:41:38 +0000

    le 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strchr - Write a function that locates a character in a string
+ * @s: pointer to the first occurrence of the character c
+ * @c: the character of the string s
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+	{
+		s++;
+	}
+	return (s);
+}

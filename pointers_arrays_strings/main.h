0f803f02a0f70e1b9c28f60600defb695ed12d6b
NChaouch <nourchaouch44@gmail.com>
2024-03-06 14:08:13 +0000

    le 4

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,5 +19,6 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
+char *_strpbrk(char *s, char *accept);
 
 #endif

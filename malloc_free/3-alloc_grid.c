255a239f54f0c6b3a4147929b8cf4ae0365e9ce3
NChaouch <nourchaouch44@gmail.com>
2024-03-13 10:58:55 +0000

    le 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,39 +10,44 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int count1;
-	int count2;
+	/* declarations of variables */
+	int **grid, count1, count2;
 
+	/* Checks if width or height is zero or negative */
 	if (width <= 0 || height <= 0)
 	{
-		return (NULL);
+		return (NULL); /* Returns NULL if dimensions are invalid */
 	}
 
+	/* Allocates memory for pointer array (for each line) */
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
-		return (NULL);
+		return (NULL); /*Returns NULL if memory allocation fails */
 	}
 
+	/* Loop to allocate memory for each line and initialize elements to 0 */
 	for (count1 = 0; count1 < height; count1++)
 	{
+		/* Allocates memory for each line of the table */
 		grid[count1] = malloc(width * sizeof(int));
 		if (grid[count1] == NULL)
 		{
+			/* Releases memory allocated for previous lines if allocation fails */
 			for (count2 = 0; count2 < count1; count2++)
 			{
 				free(grid[count2]);
 			}
-			free(grid);
-			return (NULL);
+			free(grid); /* Free memory allocated for pointer array */
+			return (NULL); /* Returns NULL on failure */
 		}
 
+		/* Initializes each line element to 0 */
 		for (count2 = 0; count2 < width; count2++)
 		{
 			grid[count1][count2] = 0;
 		}
 	}
 
-	return (grid);
+	return (grid); /* Returns the pointer to the allocated pointer array */
 }

4a641581843fd081f7701f8d758a4e0aeeb2b3a3
NChaouch <nourchaouch44@gmail.com>
2024-03-13 16:09:10 +0000

    le 1

--- more_malloc_free/0-main.c
+++ more_malloc_free/0-main.c
@@ -16,13 +16,13 @@ int main(void)
     double *d;
 
     c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
+    printf("c = %p\n", (void *)c);
     i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
+    printf("i = %p\n", (void *)i);
     f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
+    printf("f = %p\n", (void *)f);
     d = malloc_checked(INT_MAX);
-    printf("%p\n", (void *)d);
+    printf("d = %p\n", (void *)d);
     free(c);
     free(i);
     free(f);

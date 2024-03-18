b8a49d3edde40dded9faf8f0254706d0a335be65
NChaouch <nourchaouch44@gmail.com>
2024-03-18 16:04:53 +0000

    Task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,7 +3,11 @@
  * print_name - Write a function that prints a name
  * @name: pointer to name
  * @f: pointer to the function that print the name
-*/
+ * return:
+ * If f == NULL - NULL.
+ * else - name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)

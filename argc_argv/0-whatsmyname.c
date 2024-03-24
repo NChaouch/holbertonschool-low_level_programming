9c9b0632f7f8f6180c8fe46dd5c369c23b666d78
NChaouch <nourchaouch44@gmail.com>
2024-03-24 13:19:39 +0000

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: Total of number arguments passed to the executable.
+ * @argv: Array of strings containing the arguments.
+ * Return: always 0.
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Unused parameter */
+
+	if (argv != NULL && argv[0] != NULL) /* Check if the argv is not NULL */
+						/* and the first argument exist */
+
+		printf("%s\n", argv[0]); /* Print the first argument */
+
+
+	return (0); /* Success of termination */
+}

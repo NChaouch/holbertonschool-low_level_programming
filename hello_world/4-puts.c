bb2b8fbb4ef2cd2c6f8bcf826be90c8212b1e348
NChaouch <nourchaouch44@gmail.com>
2024-06-17 11:02:09 +0000

    betty, add *description, comments task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int main ()
+/**
+ *  main - program that prints exactly
+ *  Programming is like building a multilingual puzzle,
+ *  followed by a new line.
+ *  Return: 0.
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 

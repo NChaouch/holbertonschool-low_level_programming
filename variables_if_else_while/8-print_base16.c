3b86c474d22a425dd83d30aa2006a352b4f6eecd
NChaouch <nourchaouch44@gmail.com>
2024-06-18 13:53:55 +0000

    betty task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int num;
 	char letter;
-	
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
@@ -16,9 +16,9 @@ int main(void)
 	for (letter = 'a'; letter <= 'f'; letter++)
 	{
 		putchar(letter);
-	}
-	
+}
+
 	putchar('\n');
-	
+
 	return (0);
 }

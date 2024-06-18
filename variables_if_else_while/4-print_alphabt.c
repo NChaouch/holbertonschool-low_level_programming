c23854489e33db9ec613038999ee5525c8b594a7
NChaouch <nourchaouch44@gmail.com>
2024-06-18 09:15:36 +0000

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+		{
+			putchar(alph);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

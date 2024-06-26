9fd07f782e8364966d44c2fa58d73d7a1861289e
NChaouch <nourchaouch44@gmail.com>
2024-06-26 13:07:23 +0000

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - function that prints half of a string,
+ * followed by a new line.
+ * @str: pointer to the character.
+ */
+void puts_half(char *str)
+{
+	if (_strlen(str) % 2 == 0)
+	{
+		str = str + _strlen(str) / 2;
+	}
+	else
+	{
+		str = str + (_strlen(str) - 1) / 2; /* - 1 is in order to get even number */
+	}
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}

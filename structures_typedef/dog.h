9171f40ccfd10d3e5e5110c17a4352b4a65daa69
NChaouch <nourchaouch44@gmail.com>
2024-07-11 22:01:48 +0200

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type 
+ * struct dog with the following elements.
+ * @name: names of dogs.
+ * @age: age of dogs.
+ * @owner: owner of dogs.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
